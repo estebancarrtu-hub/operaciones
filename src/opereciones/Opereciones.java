7d16b356cbcd288f53a0deb25c618708a1347b22
yosepe2005 <estebancarru@gmail.com>
2026-09-22 09:02:49 +0200

    primera subida

---
+++ src/opereciones/Opereciones.java
@@ -0,0 +1,26 @@
+package opereciones;
+
+public class Opereciones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1= 3;
+		int num2= 7;
+		
+		int suma = num1+ num2;
+		
+		System.out.println("la suma es: " + suma);
+		
+		int num3= 8;
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
